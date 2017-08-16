1c8da7ab7c5d3b91593244364a6a0d269d4b1096
Ladislav Lisy <ladislav.lisy@seznam.cz>
2017-08-16 15:37:28 +0200

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,120 @@
-var gulp = require("gulp"),
-browserify = require("browserify"),
-source = require("vinyl-source-stream"),
-buffer = require("vinyl-buffer"),
-tslint = require("gulp-tslint"),
-tsc = require("gulp-typescript"),
-sourcemaps = require("gulp-sourcemaps"),
-uglify = require("gulp-uglify"),
-runSequence = require("run-sequence"),
-mocha = require("gulp-mocha"),
-istanbul = require("gulp-istanbul"),
-browserSync = require('browser-sync').create();
+"use strict";
 
+//******************************************************************************
+//* DEPENDENCIES
+//******************************************************************************
+var gulp        = require("gulp"),
+    browserify  = require("browserify"),
+    tsify       = require("tsify"),
+    source      = require("vinyl-source-stream"),
+    buffer      = require("vinyl-buffer"),
+    tslint      = require("gulp-tslint"),
+    tsc         = require("gulp-typescript"),
+    sourcemaps  = require("gulp-sourcemaps"),
+    uglify      = require("gulp-uglify"),
+    runSequence = require("run-sequence"),
+    mocha       = require("gulp-mocha"),
+    istanbul    = require("gulp-istanbul"),
+    browserSync = require('browser-sync').create();
+    
+//******************************************************************************
+//* LINT
+//******************************************************************************
 gulp.task("lint", function() {
+
+    var config =  { formatter: "verbose", emitError: (process.env.CI) ? true : false };
+    
     return gulp.src([
         "source/**/**.ts",
         "test/**/**.test.ts"
     ])
-    .pipe(tslint({}))
-    .pipe(tslint.report("verbose"));
+    .pipe(tslint(config))
+    .pipe(tslint.report());
+
 });
 
-var tsProject = tsc.createProject("tsconfig.json");
-
-gulp.task("build-app", function() {
-    return gulp.src([
-        "source/**/**.ts",
-        "typings/main.d.ts/",
-        "source/interfaces/interfaces.d.ts"
-    ])
-    .pipe(tsc(tsProject))
-    .js.pipe(gulp.dest("source/"));
-});
-
-gulp.task("bundle", function() {
-    var libraryName = "myapp";
-    var mainTsFilePath = "source/main.js";
-    var outputFolder = "dist/";
-    var outputFileName = libraryName + "min.js";
-
-    var bundler = browserify({
-        debug : true,
-        standalona : libraryName
-    });
-
-    return bundler.add(mainTsFilePath)
-        .bundle()
-        .pipe(source(outputFileName))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps : true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(outputFolder));
-});
-
-gulp.task("watch", ["default"], function() {
-    browserSync.init({
-        server : "."
-    });
-
-    gulp.watch([ "source/**/**.ts", "test/**/**.ts"], ["default"]);
-    gulp.watch("dist/*.js").on("change", browserSync.reload);
-});
-
+//******************************************************************************
+//* BUILD TEST
+//******************************************************************************
 var tsTestProject = tsc.createProject("tsconfig.json");
 
 gulp.task("build-test", function() {
     return gulp.src([
-        "test/**/*.ts",
-        "typings/main.d.ts/",
-        "source/interfaces/interfaces.d.ts"
-    ])
-    .pipe(tsc(tsTestProject))
-    .js.pipe(gulp.dest("test/"));
+            "source/**/**.ts",
+            "test/**/**.test.ts",
+            "typings/main.d.ts/",
+            "source/interfaces/interfaces.d.ts"],
+            { base: "." }
+        )
+        .pipe(tsTestProject())
+        .on("error", function (err) {
+            process.exit(1);
+        })
+        .js
+        .pipe(gulp.dest("."));
 });
 
-gulp.task("instanbul:hook", function() {
-    return gulp.src(["source/**/*.js"])
-    // covering files
-    .pipe(istanbul())
-    // force 'require' to return covered files
-    .pipe(istanbul.hookRequire());
+//******************************************************************************
+//* TEST
+//******************************************************************************
+gulp.task("istanbul:hook", function() {
+    return gulp.src(['source/**/*.js'])
+        // Covering files
+        .pipe(istanbul())
+        // Force `require` to return covered files
+        .pipe(istanbul.hookRequire());
 });
 
-gulp.task("test", ["instanbul:hook"], function() {
-    return gulp.src(["source/**/*.test.js"])
-    // covering files
-    .pipe(mocha({ ui : 'bdd' }))
-    // force 'require' to return covered files
-    .pipe(istanbul.writeReports());
+gulp.task("test", ["istanbul:hook"], function() {
+    return gulp.src('test/**/*.test.js')
+        .pipe(mocha({ui: 'bdd'}))
+        .pipe(istanbul.writeReports());
+});
+
+//******************************************************************************
+//* BUILD DEV
+//******************************************************************************
+gulp.task("build", function() {
+  
+    var libraryName = "myapp";
+    var mainTsFilePath = "source/main.ts";
+    var outputFolder   = "dist/";
+    var outputFileName = libraryName + ".min.js";
+
+    var bundler = browserify({
+        debug: true,
+        standalone : libraryName
+    });
+    
+    return bundler
+        .add(mainTsFilePath)
+        .plugin(tsify, { noImplicitAny: true })
+        .bundle()
+        .on('error', function (error) { console.error(error.toString()); })
+        .pipe(source(outputFileName))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))        
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(outputFolder));
+});
+
+//******************************************************************************
+//* DEV SERVER
+//******************************************************************************
+gulp.task("watch", ["default"], function () {
+    
+    browserSync.init({
+        server: "."
+    });
+    
+    gulp.watch([ "source/**/**.ts", "test/**/*.ts"], ["default"]);
+    gulp.watch("dist/*.js").on('change', browserSync.reload); 
+});
+
+//******************************************************************************
+//* DEFAULT
+//******************************************************************************
+gulp.task("default", function (cb) {
+    runSequence("lint", "build-test", "test", "build", cb);
 });
